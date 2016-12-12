0af7e1577433fe3f0e6f521bd14fb2c358df9eee
strothj <jason@jasons.io>
2016-12-12 07:39:11 -0500

    Added default route so other paths handled by single page app work as expected

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use((req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
+});
 
 function runServer(cb) {
   const port = process.env.PORT || 8080;
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -5,7 +5,7 @@ import { app, runServer } from '../src/main';
 chai.use(chaiHttp);
 const should = chai.should();
 
-describe('Index', () => {
+describe('Static files', () => {
 
   before((done) => {
     runServer((err) => {
@@ -17,9 +17,19 @@ describe('Index', () => {
     });
   });
 
-  it('should return status OK', (done) => {
+  it('root should return status OK', (done) => {
     chai.request(app)
       .get('/')
+      .end((err, res) => {
+        should.not.exist(err);
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it('login should return status OK', (done) => {
+    chai.request(app)
+      .get('/login')
       .end((err, res) => {
         should.not.exist(err);
         res.should.have.status(200);
